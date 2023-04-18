c3b974b1f4031f436d1304a9d89da5fa55236984
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-18 07:38:08 +0200

    Home-page Copyright dynamic year done

--- src/Components/Home/FooterBotom/FooterBotom.jsx
+++ src/Components/Home/FooterBotom/FooterBotom.jsx
@@ -33,7 +33,7 @@ const FooterBottom = () => <footer className="page-footer font-small blue pt-4">
         </div>
     </div>
 
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
+    <div className="footer-copyright text-center py-3">© {(new Date()).getFullYear()} Copyright:
         <a href="/"> Shariful Islam</a>
     </div>
 
