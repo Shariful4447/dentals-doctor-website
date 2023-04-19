43c01323e200c5595234a00a944f77c2ffb0d41c
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-19 02:15:30 +0200

    Home-page Routing with other page started

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Home from './Components/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Appointment from './Components/Appointment/Appointment/Appointment';
+
 
 
 
 const App = () => {
   return (
-    <div>
-     <Home></Home>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} exact />
+          <Route exact path='/appointment' element={<Appointment/>}/>
+        </Routes> 
+    </Router>    
   );
 };
 
