d5312ac7103061fd22f8b49e5d491df252ac52e1
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-20 23:52:18 +0200

    calender date changed function added

--- src/Components/Appointment/Appointmentheader/AppointmentHeader.jsx
+++ src/Components/Appointment/Appointmentheader/AppointmentHeader.jsx
@@ -10,42 +10,31 @@ import 'react-calendar/dist/Calendar.css';
 
 
 const AppointmentHeader = () => {
-    const handleChange=()=> {
-        console.log('date Changed');
+    const handleChange= date => {
+        console.log('date Changed', date);
     }
     return (
-        <main className={classes.bgChair}>
+        <main>
             
-            <div className ="container-fluid" style={{display:'flex'}}>
-                <div className="row">
-                    <div className ="col-md-6 col-sm-6 calender">
-                        <h1 style={{color: '#3A4256', marginTop:'40px' }}>Appointment : </h1>
-                            <Calendar onChange={handleChange} 
-                            value={new Date() } />
+            <div className='bgChair'>
+                <div className ="container-fluid" style={{display:'flex'}}>
+                    <div className="row">
+                        <div className ="col-md-6 col-sm-6 calender">
+                            <h1 style={{color: '#3A4256', marginTop:'40px' }}>Appointment : </h1>
+                                <Calendar onChange={handleChange} 
+                                value={new Date() } />
+                            
+                        </div>
+                        <div className ="col-md-6 col-sm-6 main-image">
                         
+                                <img style={{height:'335px', width:'600px', marginTop:'40px', marginRight:'0px'}} src={chair} alt="" />
+                            
+                        </div>
                     </div>
-                    <div className ="col-md-6 col-sm-6 main-image">
-                       
-                            <img style={{height:'450px', width:'600px', marginTop:'40px', marginRight:'0px'}} src={chair} alt="" />
-                        
-                    </div>
-                </div>
-            </div>  
+                </div>  
+            </div>
                
-                {/* <Row className='Content' >
-                    <div className="col-md-8">
-                        <h1 style={{color: '#3A4256' }}>Appointment : </h1>
-                        <Calendar onChange={handleChange} 
-                        value={new Date() } />
-
-                    </div>
-                    <div className="col-md-4 main-image">
-                        <img src={chair} alt=""/>
-                    </div>
-                </Row> */}
-
-            
-
+                
         </main>
     );
 };
