d181f54226110a2c0821e496444cfd1c71415d73
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-20 23:38:46 +0200

    Apointment page Calender Added

--- src/Components/Appointment/Appointmentheader/AppointmentHeader.jsx
+++ src/Components/Appointment/Appointmentheader/AppointmentHeader.jsx
@@ -4,24 +4,47 @@ import Button from 'react-bootstrap/Button';
 import classes from '../../Home/Header/Header.module.css'
 import './AppointmentHeader.css'
 import { Row } from 'react-bootstrap';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 
 const AppointmentHeader = () => {
+    const handleChange=()=> {
+        console.log('date Changed');
+    }
     return (
-        <main >
-            <div className={classes.bgChair}>
-                <Row className='Content' >
-                    <div className="col-md-8 offset-md-1">
+        <main className={classes.bgChair}>
+            
+            <div className ="container-fluid" style={{display:'flex'}}>
+                <div className="row">
+                    <div className ="col-md-6 col-sm-6 calender">
+                        <h1 style={{color: '#3A4256', marginTop:'40px' }}>Appointment : </h1>
+                            <Calendar onChange={handleChange} 
+                            value={new Date() } />
+                        
+                    </div>
+                    <div className ="col-md-6 col-sm-6 main-image">
+                       
+                            <img style={{height:'450px', width:'600px', marginTop:'40px', marginRight:'0px'}} src={chair} alt="" />
+                        
+                    </div>
+                </div>
+            </div>  
+               
+                {/* <Row className='Content' >
+                    <div className="col-md-8">
                         <h1 style={{color: '#3A4256' }}>Appointment : </h1>
-                    
+                        <Calendar onChange={handleChange} 
+                        value={new Date() } />
 
                     </div>
                     <div className="col-md-4 main-image">
                         <img src={chair} alt=""/>
                     </div>
-                </Row>
+                </Row> */}
 
-            </div>
+            
 
         </main>
     );
