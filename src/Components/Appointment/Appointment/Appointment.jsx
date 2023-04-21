53d8ba010fa90e2bd59866e1296256c074ed2589
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-21 22:42:42 +0200

    Handle date Change function added

--- src/Components/Appointment/Appointment/Appointment.jsx
+++ src/Components/Appointment/Appointment/Appointment.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppointmentHeader from '../Appointmentheader/AppointmentHeader';
 
 
 
 
 const Appointment = () => {
+  const [selectedDate, setSelectedDate] =useState(new Date());
+  const handleChange = date =>{
+    selectedDate(date);
+  }
     return (
         <div>
           <AppointmentHeader></AppointmentHeader>  
